9b7f95918ad765e76cd629772bf4eeb900688f00
haseeb ashraf <haseeb.ashraf@psca.gop.pk>
2025-08-26 17:27:23 +0500

    add navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <head>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
