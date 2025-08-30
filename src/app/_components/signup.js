f53f6e91f3a4c737399eb5e8739818745569419b
haseeb ashraf <haseeb.ashraf@psca.gop.pk>
2025-08-30 16:13:04 +0500

    oklecture 7 ok

--- src/app/_components/signup.js
+++ src/app/_components/signup.js
@@ -47,6 +47,7 @@ const Signup = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 value={formData.restaurantName}
                                 onChange={handleInputChange}
+                                required
                             />
                         </div>
                         <div>
@@ -61,6 +62,7 @@ const Signup = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 value={formData.city}
                                 onChange={handleInputChange}
+                                required
                             />
                         </div>
                         <div>
@@ -75,6 +77,7 @@ const Signup = () => {
                                 placeholder="name@company.com"
                                 value={formData.email}
                                 onChange={handleInputChange}
+                                required
                             />
                         </div>
                         <div>
@@ -89,6 +92,7 @@ const Signup = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 value={formData.address}
                                 onChange={handleInputChange}
+                                required
                             />
                         </div>
                         <div>
@@ -103,6 +107,7 @@ const Signup = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 value={formData.contactNo}
                                 onChange={handleInputChange}
+                                required
                             />
                         </div>
                         <div>
@@ -117,6 +122,7 @@ const Signup = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 value={formData.password}
                                 onChange={handleInputChange}
+                                required
                             />
                         </div>
                         <div className="flex items-start">
