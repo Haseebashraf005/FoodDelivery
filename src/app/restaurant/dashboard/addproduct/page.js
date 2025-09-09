91b7b6610cdff02a7bf5aa03606ff057a2486fc3
haseeb ashraf <haseeb.ashraf@psca.gop.pk>
2025-09-09 11:39:11 +0500

    ok till side bar but not active

---
+++ src/app/restaurant/dashboard/addproduct/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addproduct = () => {
+  return (
+    <div>Add product page</div>
+  )
+}
+
+export default addproduct
--- src/app/restaurant/dashboard/page.js
+++ src/app/restaurant/dashboard/page.js
@@ -1,49 +1,16 @@
 'use client'
-import Footer from '@/app/_components/Footer'
+
 import React from 'react'
 import "flowbite";
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { initFlowbite } from 'flowbite'; // Import Flowbite's init function
-import Dashboardbody from '@/app/_components/Dashboardbody';
+
 
 
 const Dashboard = () => {
 
-
-    let router = useRouter()
-    let [userDetails, setUserDetails] = useState({})
-
-
-    useEffect(() => {
-        // check if user log in or not  
-
-        let data = localStorage.getItem("user");
-        if (!data) {
-            router.push('/restaurant')
-        } else {
-            setUserDetails(JSON.parse(data))
-        }
-
-        // Initialize Flowbite components after DOM is updated
-        initFlowbite();
-
-    }, [])
-
-
-
-    function signOut() {
-
-        router.push('/restaurant')
-        localStorage.removeItem("user");
-
-    }
-
     return (
-      <>
-      test
-      </>
+        <>
+            Dashboard Home page
+        </>
     )
 }
 
